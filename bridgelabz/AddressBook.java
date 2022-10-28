77adfa6bc07386dc4dd74e11d048aaa1221e6fd8
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-28 13:30:03 +0530

    add new contact is added

--- bridgelabz/AddressBook.java
+++ bridgelabz/AddressBook.java
@@ -3,52 +3,55 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class AddressBook {
-    Scanner sc = new Scanner (System.in);
-    public static void main (String[]args){
-        System.out.println("Welcome to address book system program");
-        AddressBook addressBook = new AddressBook();
-        addressBook.addContact();
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program in");
+        System.out.println("You can create Contacts in the Address Book with first/last names, address, city, state, zip, phone number and email and so on");
 
+        AddressBook entry = new AddressBook();
+        entry.addContact();
     }
     public void addContact(){
-        System.out.println("create new contact");
-        System.out.println("enter  first name ");
-        String firstName= sc.next();
-        System.out.println("enter last name ");
-        String lastName= sc.next();
-        System.out.println("enter phone number ");
-        String phoneNumber = sc.next();
-        System.out.println("enter email id ");
-        String emailId = sc.next();
-        System.out.println("enter city name ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Create a New Contact!");
+        System.out.println("Enter First name -");
+        String firstName = sc.next();
+        System.out.println("Enter Last Name -");
+        String lastName = sc.next();
+        System.out.println("Enter the city -");
         String city = sc.next();
-        System.out.println("enter state name ");
+        System.out.println("Enter the state -");
         String state = sc.next();
-        System.out.println("enter zip");
+        System.out.println("Enter the Zip -");
         String zip = sc.next();
-
-        Contact newContact= new Contact();
-        Address address= new Address();
-        displayContact(newContact);
-        displayAddress(address);
-        newContact.setFirstName(firstName);
-        newContact.setLastName(lastName);
+        System.out.println("Enter the Phone Number -");
+        String phoneNumber = sc.next();
+        System.out.println("Enter the EmailID -");
+        String email = sc.next();
+        Contact newContact = new Contact();
+        Address newAddress = new Address();
         newContact.setPhoneNumber(phoneNumber);
-        newContact.setEmailId(emailId);
-        address.setCity(city);
-        address.setState(state);
-        address.setZip(zip);
-        sc.close();
-        
+        newContact.setEmailId(email);
+        newContact.setLastName(lastName);
+        newContact.setFirstName(firstName);
+        newAddress.setCity(city);
+        newAddress.setZip(zip);
+        newAddress.setState(state);
+        displayContact(newContact);
+        displayAddress(newAddress);
+
     }
+    public void displayContact(Contact contact){// contain class name and reference variable
 
-    public void displayContact(Contact contact){
-        System.out.println(contact);
-
-
+        System.out.println(contact);// (contact object) call toString (override)
     }
     public void displayAddress(Address address){
         System.out.println(address);
-
     }
 }
+
+/* Ability to add a new
+Contact to Address Book - Use Console to add person details from
+AddressBookMain class
+- Use Object Oriented Concepts to manage
+relationship between AddressBook and Contact
+Person*/
--- bridgelabz/Contact.java
+++ bridgelabz/Contact.java
@@ -47,4 +47,6 @@ public class Contact {
                 ", emailId='" + emailId + '\'' +
                 '}';
     }
-}
+
+    }
+
